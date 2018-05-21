5b95b9c1203191b476f359fdd6b7c357bb25585e
rupeshsai0446 <38848767+rupeshsai0446@users.noreply.github.com>
2018-05-21 14:23:31 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import media
+import fresh_tomatoes
+
+
+tholiprema = media.Movie("THOLIPREMA",
+                         "https://bit.ly/2GAqgKB",
+                         "https://www.youtube.com/embed/xca_8P_6yM8")
+padmavati = media.Movie("PADMAVATI",
+                        "https://bit.ly/2wYIqX4",
+                        "https://www.youtube.com/embed/X_5_BLt76c0")
+vivegam = media.Movie("VIVEGAM",
+                      "https://bit.ly/2ISTw4p",
+                      "https://www.youtube.com/embed/yJdHR8nCYWk")
+
+junglebook = media.Movie("JUNGLE BOOK",
+                         "https://bit.ly/2rWSB8P",
+                         "https://www.youtube.com/embed/5mkm22yO-bs")
+mahanati = media.Movie("MAHANATI",
+                       "https://bit.ly/2KD0hEP",
+                       "https://www.youtube.com/embed/Dtp_0ahGSfY")
+avengers = media.Movie("AVENGERS",
+                       "https://bit.ly/2qzWJtk",
+                       "https://www.youtube.com/embed/QwievZ1Tx-8")
+movies = [tholiprema, padmavati, vivegam, junglebook, mahanati, avengers]
+fresh_tomatoes.open_movies_page(movies)
+
