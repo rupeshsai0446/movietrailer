8f7077317ae225ed910db5eba0bf0ffdae91dc58
rupeshsai0446 <38848767+rupeshsai0446@users.noreply.github.com>
2018-05-23 12:19:58 +0530

    update changes

--- media.py
+++ media.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
-import webbrowser
+import webbrowser  # webbrowser module is imported.
 
 
 class Movie():
-        def __init__(self, movie_title, poster_image, trailer_youtube):
+    '''
+     here class name is taken as a movie and some arguments are taken.
+     argument1:movie_title defines the movie name.
+     argument2:poster_image defines the  image of that movie.
+     argument3:traier_youtube gives the youtube url of that moivie trailer.
+    '''
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        # initializes the space for storing the arguments.
                 self.title = movie_title
                 self.poster_image_url = poster_image
                 self.trailer_youtube_url = trailer_youtube
 
 
-def show_trailer(self):
+def show_trailer(self):  # opens the movie trailer in a updated modern browser.
             webbrowser.open(self.trailer_youtube_url)
-
